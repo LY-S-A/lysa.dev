15b7177b23f720ba6edf5d9206512dd584e507d0
lysa.dev <Lysauniverse30@gmail.com>
2025-04-24 02:53:44 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Waitlist from './waitlist';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Waitlist />
+    </div>
+  );
+}
+
+export default App;
